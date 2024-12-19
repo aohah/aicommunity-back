6290280a428eaca76e66fa3d519d56725024f87d
quling <x2995924975@163.com>
2024-12-19 17:38:33 +0800

    添加消息头

---
+++ src/main/java/com/experiment/aicommunityback/Controller/AKController.java
@@ -0,0 +1,12 @@
+package com.experiment.aicommunityback.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author：杨婉汐
+ * @name：Controller
+ * @Date：2024/12/19 17:05
+ */
+@RestController
+public class AKController {
+}
