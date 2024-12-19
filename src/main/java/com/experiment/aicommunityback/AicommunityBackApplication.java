63f1991d157bc8d6cb51419d4fb6e3845a0583d2
aohah <1138942703@qq.com>
2024-12-19 15:31:30 +0800

    第一次提交

---
+++ src/main/java/com/experiment/aicommunityback/AicommunityBackApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.aicommunityback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicommunityBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicommunityBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experiment/aicommunityback/AicommunityBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.aicommunityback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicommunityBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
